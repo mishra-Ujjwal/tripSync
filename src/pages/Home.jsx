c680cabb250f1b4c7c3f082a24562b3015dc196c
Vansh <vipultalwin99@gmail.com>
2025-08-28 11:21:55 +0530

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import HeroSection from '../components/HeroSection'
+import About from './About';
 
 const Home = () => {
     const images = [
@@ -23,6 +24,7 @@ const Home = () => {
     <section className={` bg-center bg-cover w-[100vw] h-[100vh] px-2 md:px-10 lg:px-20 `}   style={{ backgroundImage: `url("/images/image2.png")` }} >
         <div><Navbar/></div>
         <div className='sm:pt-30 pt-25'><HeroSection/></div>
+        <div> <About /> </div>
     </section>
 
   )
